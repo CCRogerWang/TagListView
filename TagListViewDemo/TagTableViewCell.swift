e78077600b2bb290ad3614ff2f53e12c369ecdd1
roger.wang <roger77622@gmail.com>
2018-06-26 23:35:44 +0800

    1. add demo menu using tableview 2. add a tableview demo using custom tableviewcell

---
+++ TagListViewDemo/TagTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TagTableViewCell.swift
+//  TagListViewDemo
+//
+//  Created by Roger on 2018/6/26.
+//  Copyright © 2018年 Ela. All rights reserved.
+//
+
+import UIKit
+
+class TagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
